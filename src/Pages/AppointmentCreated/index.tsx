7bc0138bb303404d8abb1656954809f48396b4b2
Filipe Freitas <filipe.freitas@live.com>
2021-02-15 23:16:00 -0300

    feat: add a success page on created an appointment

--- src/Pages/AppointmentCreated/index.tsx
+++ src/Pages/AppointmentCreated/index.tsx
@@ -1,8 +1,58 @@
-import React from 'react';
-import { View } from 'react-native';
+/* eslint-disable import/no-duplicates */
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
+
+  const { reset } = useNavigation();
+  const handleOkButton = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOkButton}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
